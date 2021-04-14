309c0a96793ebd66d64f2329d58a718b65a3d323
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-04-14 16:25:42 -0500

    Print a List.

--- 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _4_print_list
+class List
 {
-    class Program
+    public static List<int> CreatePrint(int size)
     {
-        static void Main(string[] args)
+        List<int> list_completed;
+
+        if (size < 0)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Size cannot be negative");
+            return (null);
         }
+        list_completed = new List<int>(size);
+        
+        if (size == 0)
+        {
+            Console.Write("\n");
+            return (list_completed);
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            list_completed.Add(i);
+            Console.Write("{0}{1}", i, i == size -1 ? "\n" : " ");
+        }
+        return (list_completed);
     }
 }
