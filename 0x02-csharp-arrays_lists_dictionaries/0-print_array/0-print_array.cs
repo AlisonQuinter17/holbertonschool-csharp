d71467a77c1ab2d81f4a24a4c3d9f8a477170b0f
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-04-14 12:17:04 -0500

    Print an array of integers.

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -4,18 +4,20 @@ class Array
 {
     public static int[] CreatePrint(int size)
     {
-        int[] array_completed = new int[size];
+        int[] array_completed;
         int i = 0;
 
-        if (size == 0)
-        {
-            Console.Write("\n");
-            return (array_completed);
-        }
         if (size < 0)
         {
             Console.WriteLine("Size cannot be negative");
             return (null);
+        }
+        
+        array_completed = new int[size];
+        if (size == 0)
+        {
+            Console.Write("\n");
+            return (array_completed);
         }
 
         while (i < size)
