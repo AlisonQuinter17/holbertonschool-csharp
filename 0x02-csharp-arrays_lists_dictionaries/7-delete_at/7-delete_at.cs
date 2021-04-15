655aa62e320b18a2826d66573daad25351f65191
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-04-15 11:47:53 -0500

    Delete at index in a list.

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _7_delete_at
+class List
 {
-    class Program
+    public static List<int> DeleteAt(List<int> myList, int index)
     {
-        static void Main(string[] args)
+        if (index >= myList.Count || index < 0)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Index is out of range");
+            return (myList);
         }
+        myList.Remove(myList[index]);
+        return (myList);
     }
 }
