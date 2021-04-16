943103353c16311dcb150e880caf530b762de0ab
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-04-16 17:19:51 -0500

    Insert node and sort nodes values.

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -5,21 +5,18 @@ class LList
 {
     public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
     {
-        LinkedListNode<int> new_node = myLList.First;
-
-        while (new_node != null)
+        LinkedListNode<int> current = myLList.First;
+        while (current != null)
         {
-            if (new_node.Value < n)
-            {
-                new_node = new_node.Next;
-            }
+            if (current.Value < n)
+                current = current.Next;
             else
             {
-                myLList.AddBefore(new_node, n);
-                return (new_node);
+                myLList.AddBefore(current, n);
+                return myLList.First;
             }
         }
-        myLList.AddFirst(n);
-        return (new_node);
+        myLList.AddLast(n);
+        return myLList.First;
     }
 }
