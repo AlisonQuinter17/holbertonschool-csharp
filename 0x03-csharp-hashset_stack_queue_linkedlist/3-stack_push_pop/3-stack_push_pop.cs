9af44c1772b4ba364eb8e3cde2e1705458e34805
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-04-15 23:51:04 -0500

    Stack Push, pop and peek.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aStack.Count); // Print the number of items in aStack
+
+        if (aStack.Count <= 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.WriteLine("Top item: {0}", aStack.Peek()); //Print the item at the top of aStack without removing it
+        }
+
+        if (aStack.Contains(search))
+        {
+            Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search)); //Print if aStack contains a given item search
+            while (aStack.Contains(search))
+            {
+                aStack.Pop(); // remove all items up to and including search
+            }
+        }
+        aStack.Push(newItem); // Add a new given item newItem to aStack
+        return (aStack);
+    }
+}
