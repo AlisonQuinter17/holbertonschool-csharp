efded2f6852943777c990a43e723bc420bd67004
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-04-16 12:39:26 -0500

    Lenght of a Linked List.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        var total = 0;
+
+        foreach (int i in myLList)
+        {
+            total += 1;
+        }
+        return (total);
+    }
+}
