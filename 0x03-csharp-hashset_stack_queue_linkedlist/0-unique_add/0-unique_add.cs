f844f51dd01760b5f29ac598548be013207ff811
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-04-15 21:19:02 -0500

    Add with HashSet.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> collection =  new HashSet<int>();
+        int total = 0;
+
+        for (int i = 0; i < myList.Count; i++)
+        {
+            collection.Add(myList[i]);
+        }
+        foreach (int j in collection)
+        {
+            total += j;
+        }
+        return (total);
+    }
+}
