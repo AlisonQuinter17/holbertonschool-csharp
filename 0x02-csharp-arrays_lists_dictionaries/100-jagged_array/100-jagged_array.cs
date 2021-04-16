b8bf27e3c04cadc2580d419402da585c3e10a064
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-04-15 19:24:11 -0500

    Jagged array.

--- 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -7,22 +7,13 @@ namespace _100_jagged_array
         static void Main(string[] args)
         { 
             int[][] jagged_array =  new int[3][];
-            jagged_array[0] = new int[]
-            {
-                0, 1, 2, 3
-            };
-            jagged_array[1] = new int[]
-            {
-                0, 1, 2, 3, 4, 5, 6
-            };
-            jagged_array[2] = new int[]
-            {
-                0, 1
-            };
+            jagged_array[0] = new int[] { 0, 1, 2, 3 };
+            jagged_array[1] = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+            jagged_array[2] = new int[] { 0, 1 };
 
             for (int i = 0; i < jagged_array.GetLength(0); i++)
             {
-                var join = string.Join(", ", jagged_array[i]);
+                var join = string.Join(" ", jagged_array[i]);
                 Console.WriteLine(join);
             }
         }
