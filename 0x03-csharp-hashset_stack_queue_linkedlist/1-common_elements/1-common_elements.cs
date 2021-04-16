c1ea36862a771ed520f6c5c150df5666e71c9953
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-04-15 22:00:22 -0500

    Common element between two lists.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> common_elements = new List<int>();
+
+        foreach (int i in list1)
+        {
+            foreach (int j in list2)
+            {
+                if (i == j)
+                {
+                    common_elements.Add(i);
+                }
+            }
+        }
+        common_elements.Sort();
+        return (common_elements);
+    }
+}
