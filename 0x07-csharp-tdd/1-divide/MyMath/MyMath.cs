e4f37e60c0d2aea13a6718bb5484204fa1664533
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-04-30 00:49:21 -0500

    Test done

--- 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -2,28 +2,34 @@
 
 namespace MyMath
 {
-    ///<summary>method divide members matrix</summary>
+    /// <summary> Public class. </summary>
     public class Matrix
     {
-        /// <summary>divide</summary>
-        /// <param name="matrix">Array of integers.</param>
-        /// <param name="num">number to divide.</param>
-        /// <returns>a new matrix containing divided elements.</returns>
+        /// <summary> Prototype. </summary>
+        /// <param name="matrix"> Array of integers. </param>
+        /// <param name="num"> Number to divide. </param>
+        /// <returns> A new matrix containing divided elements. </returns>
         public static int[,] Divide(int[,] matrix, int num)
         {
-            if(num == 0)
+            if (num == 0)
             {
                 Console.WriteLine("Num cannot be 0");
-                return(null);
+                return (null);
             }
-            else if(matrix == null)
-                return(null);
+            else if (matrix == null)
+            {
+                return (null);
+            }
             else
             {
-                for(int x = 0; x < matrix.GetLength(0); x++)
-                    for(int y = 0; y < matrix.GetLength(1); y++)
-                        matrix[x,y] = matrix[x,y] / num;
-                return(matrix);
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = matrix[i, j] / num;
+                    }
+                }
+                return (matrix);
             }
         }
     }
