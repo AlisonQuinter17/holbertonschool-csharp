1622fdd08700ab8874561900c98a91946177a30e
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-04-29 23:47:37 -0500

    Tests

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -2,17 +2,41 @@ using NUnit.Framework;
 
 namespace MyMath.Tests
 {
-    public class Tests
+    [TestFixture]
+    /// <summary>Tests Alz</summary>
+    public class MatrixTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void positivetwo()
         {
+            int[,] matrix = new int[,] {{ 30, 40 }, { 5, 11 }, { 15, 6 }, { 7, 8 }};
+            int n = 2;
+
+            int[,] result = Matrix.Divide(matrix, n);
+
+            Assert.AreEqual(new int[,] {{ 15, 20 }, { 2, 5 }, { 7, 3 }, { 3, 4 }}, result);
         }
 
         [Test]
-        public void Test1()
+        public void zero()
         {
-            Assert.Pass();
+            int[,] matrix = new int[,] {{ 30, 40 }, { 5, 11 }, { 15, 6 }, { 7, 8 }};
+            int n = 0;
+
+            int[,] result = Matrix.Divide(matrix, n);
+
+            Assert.AreEqual(null, result);
+        }
+        
+        [Test]
+        public void snull()
+        {
+            int[,] matrix = null;
+            int n = 3;
+
+            int[,] result = Matrix.Divide(matrix, n);
+
+            Assert.AreEqual(null, result);
         }
     }
 }
