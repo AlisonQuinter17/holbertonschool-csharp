51c4168f5d690f3433847cbeb537c9ce5ef9ad47
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-02 16:43:38 -0500

    Tests

--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -2,7 +2,27 @@
 
 namespace MyMath
 {
-    public class Class1
+    /// <summary> Public class. </summary>
+    public class Operations
     {
+        /// <summary> Prototype. </summary>
+        /// <param name="nums"> List of nums. </param>
+        /// <returns> Max integer in list. </returns>
+        public static int Max(List<int> nums)
+        {
+            int max = 0;
+
+            if (nums == null)
+            {
+                return (0);
+            }
+            nums.sort();
+
+            foreach (int i in nums)
+            {
+                max = i;
+            }
+            return (max);
+        }
     }
 }
