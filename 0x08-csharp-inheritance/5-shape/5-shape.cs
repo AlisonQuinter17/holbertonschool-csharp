235e48cbc8d95e5e75ea97ba239d370b455f13fb
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-03 18:04:05 -0500

    Throw when a requested method or operation is not implemented.

--- 0x08-csharp-inheritance/5-shape/5-shape.cs
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -3,7 +3,7 @@
 /// <summary> Class Shape. </summary>
 class Shape
 {
-    /// <summary> Basic shapes. </summary>
+    /// <summary> The exception that is thrown when a requested method or operation is not implemented. </summary>
     public virtual int Area()
     {
         throw new NotImplementedException("Area() is not implemented");
