17aa23c5fba60fa388a9ae1404ca2a17b5c778b2
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-02 20:34:30 -0500

    Str reversed

--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -6,7 +6,19 @@ namespace Text
     {
         public static bool IsPalindrome(string s)
         {
-            
+            string lower_string = s.ToLower();
+            lower_string = Regex.Replace(lower_string, "[:;,. \t\n\r]", "");
+            char[] s_reversed = lower_string.ToCharArray();
+            Array.Reverse(s_reversed);
+
+            if (s == s_reversed || s == "")
+            {
+                return (true);
+            }
+            else
+            {
+                return (false);
+            }
         }
     }
 }
