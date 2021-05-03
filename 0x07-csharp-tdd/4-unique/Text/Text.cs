6a7b7e7aa96509ae53370d6d78f937e8a55c9fb6
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-02 23:21:09 -0500

    UniqueChar

--- 0x07-csharp-tdd/4-unique/Text/Text.cs
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -8,23 +8,33 @@ namespace Text
     {
         /// <summary> Prototype. </summary>
         /// <param name="s"> String to reverse. </param>
-        /// <returns> True if string is a palindrome, False if it’s not. </returns>
+        /// <returns> Index of first non-repeating character or -1 if there is no non-repeating character. </returns>
 
-        public static bool IsPalindrome(string s)
+        public static int UniqueChar(string s)
         {
-            s = s.ToLower();
-            s = Regex.Replace(s, "[:;,. \t\n\r]", "");
-            char[] s_reversed = s.ToCharArray();
-            Array.Reverse(s_reversed);
-
-            for (int compare = 0; compare < s.Length; compare++)
+            if (s == "" || s == null)
             {
-                if (s[compare] != s_reversed[compare])
-                {
-                    return (false);
-                }
+                return (-1);
             }
-            return (true);
+            s = s.ToLower();
+            char[] arr = s.ToCharArray();
+            int x;
+            
+            for (int y = 0; y <= arr.Length - 1; y++)
+            {
+                x = 0;
+                for (int z = 0; z <= arr.Length - 1; z++)
+                {
+                    if (y != z)
+                        if (arr[y] == arr[z])
+                        {
+                            x += 1;
+                        }
+                }
+                if (x == 0)
+                    return (y);
+            }
+            return (-1);
         }
     }
 }
