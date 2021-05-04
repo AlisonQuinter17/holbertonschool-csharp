1ec1e06eb63250e1561b6883272fd4b3fdfae292
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-03 19:24:35 -0500

    What happens if you try to assign a Square’s width and height directly.

--- 0x08-csharp-inheritance/9-shape/9-main.cs
+++ 0x08-csharp-inheritance/9-shape/9-main.cs
@@ -2,6 +2,7 @@ using System;
 
 class Program
 {
+    // Task 9
     static void Main(string[] args)
     {
         Rectangle aRect = new Rectangle();
@@ -21,4 +22,26 @@ class Program
         Console.WriteLine("aSquare area: {0}", aSquare.Area());
         Console.WriteLine(aSquare.ToString());
     }
+
+    //Task 10
+    /* static void Main(string[] args)
+    {
+        Square aSquare = new Square();
+
+        try
+        {
+            aSquare.Width = 12;
+            aSquare.Height = 8;
+
+            Console.WriteLine("aSquare width: {0}", aSquare.Width);
+            Console.WriteLine("aSquare height: {0}", aSquare.Height);
+            Console.WriteLine("aSquare size: {0}", aSquare.Size);
+            Console.WriteLine("aSquare area: {0}", aSquare.Area());
+            Console.WriteLine(aSquare.ToString());
+        }
+        catch (Exception e)
+        {            
+            Console.WriteLine(e);
+        }
+    }*/
 }
