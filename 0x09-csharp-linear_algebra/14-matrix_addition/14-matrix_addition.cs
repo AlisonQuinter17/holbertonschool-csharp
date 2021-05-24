91eb6e8d1c6d8eeff29e77823abce480527d830d
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-24 16:16:00 -0500

    Matrix addition

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -12,7 +12,7 @@ class MatrixMath
         {
             for (int i = 0; i < matrix1.GetLength(0); i++)
             {
-                for (int j = 0; j < matrix2.GetLength(1); j++)
+                for (int j = 0; j < matrix1.GetLength(1); j++)
                 {
                     matrix1[i, j] = matrix1[i, j] + matrix2[i, j];
                 }
