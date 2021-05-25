c76a2209ed6c5c90f5c38c06564765ca7d6d3b24
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-25 11:06:29 -0500

    Matrix * Matrix

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -6,8 +6,7 @@ class MatrixMath
     /// <summary> Multiplies two matrices and returns the resulting matrix. </summary>
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
-        double[,] error = { { -1 } };
-        double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+        double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)], error = { { -1 } };
         double total;
 
         if (matrix1.GetLength(1) == matrix2.GetLength(0))
