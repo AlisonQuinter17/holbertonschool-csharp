4120cc3d3e651806b7f7bab5cafee13e47a66e79
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-25 10:58:15 -0500

    Matrix * Matrix

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -6,8 +6,9 @@ class MatrixMath
     /// <summary> Multiplies two matrices and returns the resulting matrix. </summary>
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
-        double[,] empty_matrix = { { -1 } };
-        double[,] matrix3 = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+        double[,] error = { { -1 } };
+        double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+        double total;
 
         if (matrix1.GetLength(1) == matrix2.GetLength(0))
         {
@@ -15,17 +16,17 @@ class MatrixMath
             {
                 for (int j = 0; j < matrix2.GetLength(1); j++)
                 {
-                    if (j == 0)
-                        matrix3[i, j] = (matrix1[i, j] * matrix2[0, j]) + (matrix1[i, j + 1] * matrix2[1, j]);
-                    else
-                        matrix3[i, j] = (matrix1[i, j] * matrix2[1, j]) + (matrix1[i, j - 1] * matrix2[0, j]);
+                    total = 0;
+                    for (int k = 0; k < matrix1.GetLength(1); k++)
+                    {
+                        total += (matrix2[k, j]) * (matrix1[i, k]);
+                    }
+                    result[i, j] = total;
                 }
             }
-            return (matrix3);
+            return (result);
         }
         else
-        {
-            return (empty_matrix);
-        }
+            return (error);
     }
 }
