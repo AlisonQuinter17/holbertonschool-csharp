4eef455c5f8bf5d147a3045141b9065300ab97e1
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-26 15:23:24 -0500

    Determinant of a matrix

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -10,13 +10,13 @@ class MatrixMath
 
         if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
         {
-            return (Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0])));
+            return (Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2));
         }
         else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
         {
             left = ((matrix[0, 0] * matrix[1, 1] * matrix[2, 2]) + (matrix[1, 0] * matrix[2, 1] * matrix[0, 2]) + (matrix[2, 0] * matrix[0, 1] * matrix[1, 2]));
             right = ((matrix[0, 2] * matrix[1, 1] * matrix[2, 0]) + (matrix[1, 2] * matrix[2, 1] * matrix[0, 0]) + (matrix[2, 2] * matrix[0, 1] * matrix[1, 0]));
-            return (Math.Round(left - right));
+            return (Math.Round((left - right), 2));
         }
         else
             return (-1);
