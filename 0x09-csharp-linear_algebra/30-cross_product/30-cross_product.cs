7ebf7f0970a1fe5a449c45c3339e7f68054782c4
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-26 16:46:27 -0500

    Croos product

--- 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -7,12 +7,12 @@ class VectorMath
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
         if (vector1.Length != 3 || vector2.Length != 3)
-            return (new double[] { -1 });
+            return new double[] {-1};
 
-        double[] answer = new double[3];
-        answer[0] = (vector1[1] * vector2[2]) - (vector1[2] * vector2[1]);
-        answer[1] = (vector1[0] * vector2[2]) - (vector1[2] * vector2[0]);
-        answer[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
-        return (answer);
+        double[] rt = new double[3];
+        rt[0] = (vector1[1] * vector2[2]) - (vector1[2] * vector2[1]);
+        rt[1] = (vector1[2] * vector2[0]) - (vector1[0] * vector2[2]);
+        rt[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
+        return rt;
     }
 }
