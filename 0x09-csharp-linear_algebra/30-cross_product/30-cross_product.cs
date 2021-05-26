679b68cfc234269459874a4b5cc7102b130b1a70
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-26 16:27:29 -0500

    Cross product

--- 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -9,13 +9,13 @@ class VectorMath
         double[,] matrix_i = { { vector1[1], vector1[2] }, { vector2[1], vector2[2] } };
         double[,] matrix_j = { { vector1[0], vector1[2] }, { vector2[0], vector2[2] } };
         double[,] matrix_k = { { vector1[0], vector1[1] }, { vector2[0], vector2[1] } };
-        double[] answer = { 0, 0, 0 };
+        double[] answer = new double[3];
 
         if (vector1.Length == 3 && vector2.Length == 3)
         {
-            answer[0] = Math.Round((matrix_i[0, 0] * matrix_i[1, 1]) - (matrix_i[0, 1] * matrix_i[1, 0]), 2);
-            answer[1] = Math.Round((matrix_j[0, 0] * matrix_j[1, 1]) - (matrix_j[0, 1] * matrix_j[1, 0]), 2);
-            answer[2] = Math.Round((matrix_k[0, 0] * matrix_k[1, 1]) - (matrix_k[0, 1] * matrix_k[1, 0]), 2);
+            answer[0] = (matrix_i[0, 0] * matrix_i[1, 1]) - (matrix_i[0, 1] * matrix_i[1, 0]);
+            answer[1] = (matrix_j[0, 0] * matrix_j[1, 1]) - (matrix_j[0, 1] * matrix_j[1, 0]);
+            answer[2] = (matrix_k[0, 0] * matrix_k[1, 1]) - (matrix_k[0, 1] * matrix_k[1, 0]);
             return (answer);
         }
         else
