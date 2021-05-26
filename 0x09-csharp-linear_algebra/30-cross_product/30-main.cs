3f427d4822005eb04980ea31d2e5967af4ebe2e7
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-26 16:57:00 -0500

    Cross Product

--- 0x09-csharp-linear_algebra/30-cross_product/30-main.cs
+++ 0x09-csharp-linear_algebra/30-cross_product/30-main.cs
@@ -4,11 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        double[] vector1 = { 2, -2, 1 };
-        double[] vector2 = { -8, 8, -4 };
-        double[] answer;
+        double[] vector1 = { 2, -2, 1 }, vector2 = { -8, 8, -4 };
+        double[] vector3 = { -3, -2, 5 }, vector4 = { 6, -10, -1 };
+        double[] answer1, answer2;
 
-        answer = VectorMath.CrossProduct(vector1, vector2);
-        Console.WriteLine("({0}, {1}, {2})", answer[0], answer[1], answer[2]);
+        answer1 = VectorMath.CrossProduct(vector1, vector2);
+        Console.WriteLine("({0}, {1}, {2})", answer1[0], answer1[1], answer1[2]);
+
+        answer2 = VectorMath.CrossProduct(vector3, vector4);
+        Console.WriteLine("({0}, {1}, {2})", answer2[0], answer2[1], answer2[2]);
     }
 }
