97d16a958e9ba360e17489895cf078f998d5b0c2
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-26 16:37:09 -0500

    Cross Product

--- 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -10,9 +10,9 @@ class VectorMath
 
         if (vector1.Length == 3 && vector2.Length == 3)
         {
-            answer[0] = vector1[1] * vector2[2] - vector1[2] * vector2[1];
-            answer[1] = vector1[0] * vector2[2] - vector1[2] * vector2[0];
-            answer[2] = vector1[0] * vector2[1] - vector1[1] * vector2[0];
+            answer[0] = (vector1[1] * vector2[2]) - (vector1[2] * vector2[1]);
+            answer[1] = (vector1[0] * vector2[2]) - (vector1[2] * vector2[0]);
+            answer[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
             return (answer);
         }
         else
