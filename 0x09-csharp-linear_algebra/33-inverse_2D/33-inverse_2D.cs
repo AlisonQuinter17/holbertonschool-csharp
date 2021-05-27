61c1438506b1a7aac1dc864f6b436036965b8821
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-26 19:24:27 -0500

    Inverse 2D

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -6,26 +6,25 @@ class MatrixMath
     /// <summary> Calculates the inverse of a 2D matrix and returns the resulting matrix. </summary>
     public static double[,] Inverse2D(double[,] matrix)
     {
-        double determinat;
+        double determinat, div_det;
         double [,] error = new double[,] { { -1 } };
-        double [,] answer = new double[2, 2];
-        double [,] inverse = {{matrix[1, 1], -1 * matrix[0, 1]}, {-1 * matrix[1, 0], matrix[0, 0]}};
+        double [,] answer = new double[2, 2], inverse;
         
         if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
             return (error);
 
+        inverse = new double[,]{{matrix[1, 1], -1 * matrix[0, 1]}, {-1 * matrix[1, 0], matrix[0, 0]}};
         determinat = Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2);
 
         if (determinat == 0)
             return (error);
 
-        double divDet = 1 / determinat;
-
+        div_det = 1 / determinat;
         for (int i = 0; i < 2; i++)
         {
             for (int j = 0; j < 2; j++)
             {
-                answer[i, j] = Math.Round(divDet * inverse[i, j], 2);
+                answer[i, j] = Math.Round(div_det * inverse[i, j], 2);
             }
         }
         return (answer);
