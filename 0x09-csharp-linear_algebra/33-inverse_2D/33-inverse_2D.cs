09c83e1918a716f08ab55e3eccdf3292b020f2a2
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-26 19:02:49 -0500

    Inverse 2D

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -9,13 +9,13 @@ class MatrixMath
         double[,] answer = new double[2, 2], error = new double[,] { { -1 } };
         double determinat = Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2);
 
-        answer[0, 0] = matrix[1, 1];
-        answer[0, 1] = -1 * matrix[0, 1];
-        answer[1, 0] = -1 * matrix[1, 0];
-        answer[1, 1] = matrix[0, 0];
-
         if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2 && determinat != 0)
         {
+            answer[0, 0] = matrix[1, 1];
+            answer[0, 1] = -1 * matrix[0, 1];
+            answer[1, 0] = -1 * matrix[1, 0];
+            answer[1, 1] = matrix[0, 0];
+
             for (int i = 0; i < answer.GetLength(0); i++)
             {
                 for (int j = 0; j < answer.GetLength(1); j++)
