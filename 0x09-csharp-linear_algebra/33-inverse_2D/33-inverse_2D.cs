dc22f4a48138cbee29635f7645c94394aab6c0e3
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-26 19:26:35 -0500

    Inverse 2D

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -6,7 +6,7 @@ class MatrixMath
     /// <summary> Calculates the inverse of a 2D matrix and returns the resulting matrix. </summary>
     public static double[,] Inverse2D(double[,] matrix)
     {
-        double determinat, div_det;
+        double determinat;
         double [,] error = new double[,] { { -1 } };
         double [,] answer = new double[2, 2], inverse;
         
@@ -19,12 +19,11 @@ class MatrixMath
         if (determinat == 0)
             return (error);
 
-        div_det = 1 / determinat;
         for (int i = 0; i < 2; i++)
         {
             for (int j = 0; j < 2; j++)
             {
-                answer[i, j] = Math.Round(div_det * inverse[i, j], 2);
+                answer[i, j] = Math.Round(inverse[i, j] / determinat, 2);
             }
         }
         return (answer);
